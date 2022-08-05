5fb79a79c13942c9b3bbbffa088893b12c520584
pporong <inane09@gmail.com>
2022-08-05 18:55:45 +0900

    JAM 시작

---
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -0,0 +1,9 @@
+package com.KoreaIT.example.JAM;
+
+import javax.management.openmbean.OpenDataException;
+
+public class Article {
+	int id;
+	String title;
+	String body;
+}
---
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -0,0 +1,22 @@
+package com.KoreaIT.example.JAM;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.println(" 명령어 : ");
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.equals("exit")) {
+				System.out.println("== 프로그램을 종료합니다 ==");
+				break;
+			}
+		}
+
+	}
+
+}
