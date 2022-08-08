c4afab3b747370d1019815763e9a4956ffd12288
pporong <inane09@gmail.com>
2022-08-08 16:48:07 +0900

    기본적인 게시물 작성, 리스트 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,4 +1,7 @@
 package com.KoreaIT.example.JAM;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,9 +10,41 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
+
 		while (true) {
-			System.out.println(" 명령어 : ");
+			System.out.printf("명령어 : ");
 			String cmd = sc.nextLine().trim();
+
+			if (cmd.equals("article write")) {
+				System.out.println("< 게시물 작성 >");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+
+				System.out.println(article);
+
+//				System.out.printf("%d번 글 \n", article.id);
+			} else if (cmd.equals("article list")) {
+				System.out.println("< 게시물 목록 >");
+
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다. :( ");
+					continue;
+				}
+				System.out.println("번호 / 제목");
+
+				for (Article article : articles) {
+					System.out.printf("%d  / %s \n", article.id, article.title);
+				}
+			}
 
 			if (cmd.equals("exit")) {
 				System.out.println("== 프로그램을 종료합니다 ==");
