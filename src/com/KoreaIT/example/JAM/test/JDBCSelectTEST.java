e45c99722e2e7ab3d97b72bed220ba22bf0292cc
pporong <inane09@gmail.com>
2022-08-09 16:47:09 +0900

    JDBC SELECT 테스트

--- src/com/KoreaIT/example/JAM/test/JDBCSelectTEST.java
+++ src/com/KoreaIT/example/JAM/test/JDBCSelectTEST.java
@@ -3,39 +3,64 @@ package com.KoreaIT.example.JAM.test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class JDBCInsertTEST {
+import com.KoreaIT.example.JAM.Article;
+
+public class JDBCSelectTEST {
 	public static void main(String[] args) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		List<Article> articles = new ArrayList<>();
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
 			conn = DriverManager.getConnection(url, "root", "");
-			System.out.println("연결 성공!");
+			System.out.println("연결 성공 !!");
 
-			String sql = "INSERT INTO article";
-			sql += " SET regDate = NOW()";
-			sql += ", updateDate = NOW()";
-			sql += ", title = CONCAT('제목',RAND())";
-			sql += ", `body` = CONCAT('내용',RAND());";
+			String sql = "SELECT * ";
+			sql += " FROM article";
+			sql += " ORDER BY id DESC";
 
 			System.out.println(sql);
 
 			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
 
-			int affectedRows = pstmt.executeUpdate();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String regDate = rs.getString("regDate");
+				String updateDate = rs.getString("updateDate");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
 
-			System.out.println("affectedRows : " + affectedRows);
+				Article article = new Article(id, regDate, updateDate, title, body);
+
+				articles.add(article);
+			
+			}
+
+//			System.out.println("affectedRows : " + affectedRows);
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
 			System.out.println("에러: " + e);
 		} finally {
+			try {
+				if (rs != null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 			try {
 				if (pstmt != null && !pstmt.isClosed()) {
 					pstmt.close();
@@ -51,5 +76,6 @@ public class JDBCInsertTEST {
 				e.printStackTrace();
 			}
 		}
+		System.out.println("결과 : " + articles);
 	}
 }
